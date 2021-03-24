92921951b0690d99efa887fc28ae40d0a2004afe
Emmanuel Johua <jo4christ48@yahoo.com>
2021-03-24 14:22:03 +0100

    Cleaned up some bugs
    Removed some un-used vars, uninstalled un-used module (axios)

--- src/js/utils/utilities.js
+++ src/js/utils/utilities.js
@@ -1,7 +1,3 @@
-import axios from 'axios';
-const randomInt = (max, min) =>{
-    return Math.floor(Math.random() * (max-min) + min);
-};
 
 const getLocalTheme = themeId =>{
     return window.localStorage.getItem(themeId);
@@ -11,15 +7,5 @@ const setLocalTheme = (themeId, themeValue) =>{
     window.localStorage.setItem(themeId, themeValue);
 };
 
-//fetch random quote from https://api.quotable.io/random
 
-function fetchQuote(){
-   // fetch a random
-    fetch('https://api.quotable.io/random')
-    .then(response => response.json())
-    .then(quote =>  quote)
-    .catch(err =>{});
-};
-
-export default randomInt;
-export {getLocalTheme, setLocalTheme, fetchQuote};
+export {getLocalTheme, setLocalTheme};
